08360e65eaeb634f77efcc17192ea89a3f7dc64d
regexistaken <32675731+AnkitGupta17134@users.noreply.github.com>
2019-10-01 22:26:00 +0530

    Update Perimeter of Square
    Adding the function of Perimeter in OOPS.

--- Square.java
+++ Square.java
@@ -1,1 +1,19 @@
+class Square
+{
 
+  int a;
+  public Square(int a)
+  {
+    this.a = a;
+  }
+
+  public long Area()
+  {
+    return Math.pow(this.a,2);
+  }
+  
+  public long Perimeter()
+  {
+    return 4*a;
+  }
+}
