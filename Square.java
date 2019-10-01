5a15cabeb9ba0de2eb5a2e221daa5c2c848d3a11
regexistaken <32675731+AnkitGupta17134@users.noreply.github.com>
2019-10-01 22:28:59 +0530

    Update Diagonal Length of Sqaure
    Adding Diagonal length of Square.

--- Square.java
+++ Square.java
@@ -1,1 +1,24 @@
+class Square
+{
 
+  int a;
+  public Square(int a)
+  {
+    this.a = a;
+  }
+
+  public long Area()
+  {
+    return Math.pow(this.a,2);
+  }
+
+  public long Perimeter()
+  {
+    return 4*a;
+  }
+  
+  public double Diagonal()
+  {
+    return Math.sqrt((a*a)+(a*a));
+  }
+}
